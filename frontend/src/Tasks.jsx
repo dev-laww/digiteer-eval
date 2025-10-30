5c85845dbb29991548d4982d3f5991753f1311db
laww <lawrenceallen.tech@gmail.com>
2025-10-30 23:35:46 +0800

    remove task fetching logic and rendering from Tasks component

--- frontend/src/Tasks.jsx
+++ frontend/src/Tasks.jsx
@@ -1,27 +1,12 @@
-import { useEffect, useState } from 'react';
-import api from "./api/axios"
-
 function Tasks() {
-  const [tasks, setTasks] = useState([]);
+    return (
+        <div>
+            <h2>Tasks</h2>
+            <ul>
 
-  useEffect(() => {
-    api.get('/tasks')
-      .then(res => setTasks(res.data))
-      .catch(err => console.error(err));
-  }, []);
-
-  return (
-    <div>
-      <h2>Tasks</h2>
-      <ul>
-        {tasks.map(task => (
-          <li key={task.id}>
-            {task.title} {task.isDone ? '✅' : '❌'}
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+            </ul>
+        </div>
+    );
 }
 
 export default Tasks;
