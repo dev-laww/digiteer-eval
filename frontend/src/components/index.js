d161935599b9afc14dd482561d69c3607fa50f33
laww <lawrenceallen.tech@gmail.com>
2025-10-30 23:36:34 +0800

    add exports for UI components and authentication forms

---
+++ frontend/src/components/index.js
@@ -0,0 +1,21 @@
+export { Providers } from './Providers.jsx'
+
+// UI
+export { Button } from './ui/button.jsx'
+export { Label } from './ui/label.jsx'
+export { 
+  Card,
+  CardHeader,
+  CardFooter,
+  CardTitle,
+  CardAction,
+  CardDescription,
+  CardContent,
+} from './ui/card.jsx'
+
+// Auth Forms
+export { default as LoginForm } from './auth/LoginForm.jsx'
+export { default as RegisterForm } from './auth/RegisterForm.jsx'
+export { default as ProtectedRoute } from './ProtectedRoute.jsx'
+
+
